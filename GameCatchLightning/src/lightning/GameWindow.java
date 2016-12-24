01f422b4af0bebd2a175e7d1eac968cc3f30a5bd
Lubchyk <lubchyk@yandex.ua>
2016-12-24 20:48:57 +0200

    Create class "Game Window"

--- GameCatchLightning/src/lightning/GameWindow.java
+++ GameCatchLightning/src/lightning/GameWindow.java
@@ -1,10 +1,39 @@
 package lightning;
 
-public class GameWindow {
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+public class GameWindow extends JFrame{
+	
+	private static GameWindow gameWindow;
 
 	public static void main(String[] args) {
-		// TODO Автоматически созданная заглушка метода
-
+		gameWindow = new GameWindow();
+		gameWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		gameWindow.setLocation(150, 25);
+		gameWindow.setSize(1000, 700);
+		gameWindow.setResizable(false);
+		GamePanel gamePanel = new GamePanel();
+		gameWindow.add(gamePanel);
+		gameWindow.setVisible(true);
+		
 	}
-
+	private static  void onRepaind(Graphics g) {
+		g.fillOval(50, 20, 150, 30);
+		g.drawLine(0, 600, 1000, 600);
+		
+	}
+	private static class GamePanel extends JPanel{
+		@Override
+		protected void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			onRepaind(g);
+			
+		}
+		
+		
+	}
 }
